aba491fd765274ae3144a100bca95932efae165d
kavitasagade <68231212+kavitasagade@users.noreply.github.com>
2020-07-25 15:22:47 +0530

    Add files via upload

---
+++ Day6 Assignment/ass2.js
@@ -0,0 +1,18 @@
+function printTable(num){
+    var input = num.value;
+    if(!isNaN(input)) {
+        var table="";
+        var number="";
+        for(i=1;i<=10;i++) {
+            number = input * i;
+            table += input + " * " + i  + " = " + number + "<br/>";
+
+        }
+        document.getElementById("mul").innerHTML = table;
+
+    }
+    else{
+        document.getElementById("mul").innerHTML = "please enter a valid number";
+    }
+
+}
